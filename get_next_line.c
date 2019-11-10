f0703e8c11d85ee35d290647a135ef2955cc6fa9
alexsdosantos <54394628+alexsdosantos@users.noreply.github.com>
2019-11-10 15:58:22 +0100

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -1,14 +1,87 @@
 #include "get_next_line.h"
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int	ft_strlen(const char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2, int size)
+{
+	int		i;
+	int		j;
+	char	*str;
+	int len_s1;	
+
+	i = 0;
+	j = 0;
+	len_s1 = 0;
+	while (s1)
+		len_s1++;
+	if (!(str = malloc(sizeof(char) * (len_s1 + size + 1))))
+		return (NULL);
+	while (s1)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0' && j < size)
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
+}
+
+int	check(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] && s[i] != '\n')
+		i++;
+	return (i);
+}
+
 int	get_next_line(int fd, char **line)
 {
 	char buf[BUFFER_SIZE];
 	int ret;
+	char *tmp;
+	int			i; 
+	static int val;
 
-	while (ret = read(fd, buf, BUFF_SIZE))
+	tmp = 0;
+	ret = 1;
+	i = BUFFER_SIZE;
+	printf("%s\n", *line);
+	while ((i == BUFFER_SIZE || buf[i] != '\n') && ret > 0)
+	{
+		if ((ret = read(fd, buf, BUFFER_SIZE)) > 0)
 		{
-		
+			buf[ret] = '\0';
+			i = check(buf);
+			if ((tmp = ft_strjoin(*line, buf, i)))
+			{
+				if (*line)
+					free(*line);
+				*line = tmp;
+			}
+			else
+				ret = -2;
 		}
+	}
+	val = check() + 1;
+	return (ret);
 }
 
 int		main(int ac, char **av)
@@ -24,10 +97,8 @@ int		main(int ac, char **av)
 	while ((ret = get_next_line(fd, &line)) > 0)
 	{
 		printf("%d -> [%s]\n", i, line);
-		free(line);
 		i++;
 	}
-	free(line);
 	close(fd);
 	return (0);
 }
