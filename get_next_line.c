dd3d5cc4eb4f4520a462f95e0702f33966a75a0b
Alex 42 <54394628+AlexDos-42@users.noreply.github.com>
2019-11-29 14:38:16 +0100

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: alesanto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/12 17:54:08 by alesanto          #+#    #+#             */
-/*   Updated: 2019/11/19 18:23:14 by alesanto         ###   ########.fr       */
+/*   Updated: 2019/11/29 14:34:53 by alesanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,15 @@
 static int			ft_exit(char *buffer, char *stock)
 {
 	if (buffer)
+	{
+		buffer = NULL;
 		free(buffer);
+	}
 	if (stock)
+	{
+		stock = NULL;
 		free(stock);
+	}
 	return (-1);
 }
 
@@ -69,10 +75,7 @@ int					ft_read(int fd, char *buffer, char *stock[fd])
 	while (!ft_strchr(buffer, '\n') && (ret = read(fd, buffer, BUFFER_SIZE)))
 	{
 		if (ret == -1)
-		{
-			free(buffer);
 			return (-1);
-		}
 		buffer[ret] = '\0';
 		if (!(tmp = ft_strjoin(stock[fd], buffer)))
 			return (-1);
@@ -87,24 +90,23 @@ int					get_next_line(int fd, char **line)
 	char		*buffer;
 	int			ret;
 	int			i;
-	static char	*stock[10240];
+	static char	*stock[1024];
 	char		*tmp;
 
-	if ((i = 0) || fd < 0 || fd > 10240 || BUFFER_SIZE <= 0 || !line
-		|| !(buffer = (char *)malloc(sizeof(char ) * (BUFFER_SIZE + 1))))
-		return (ft_exit);
-	ft_bzero(buffer, BUFFER_SIZE + 1);
+	if ((i = 0) || fd < 0 || fd > 1024 || BUFFER_SIZE < 1 || !line
+		   || (!(buffer = ft_calloc((sizeof(char)), (BUFFER_SIZE + 1)))))
+		return (ft_exit(buffer, stock[fd]));
 	if (!stock[fd])
 		stock[fd] = ft_calloc(0, 0);
 	if ((ret = ft_read(fd, buffer, stock)) == -1)
-		return (ft_exit);
+		return (ft_exit(buffer, stock[fd]));
 	while (stock[fd][i] && stock[fd][i] != '\n')
 		i++;
 	ret = ((stock)[fd][i] == '\n' ? 1 : 0);
 	if (!(*line = ft_substr(stock[fd], 0, i)))
-		return (ft_exit);
+		return (ft_exit(buffer, stock[fd]));
 	if (!(tmp = ft_strdup(stock[fd][i] ? stock[fd] + i + 1 : stock[fd] + i)))
-		return (ft_exit);
+		return (ft_exit(buffer, stock[fd]));
 	free(stock[fd]);
 	stock[fd] = tmp;
 	return (ret);
